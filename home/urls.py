48943519af5d97f52720ccefdfbd89fdf7952fba
Bora Shin <d6ymmd@gmail.com>
2020-02-06 10:48:00 +0900

    notice & portfolio list

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+from django.urls import path, include
 from home import views
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('notice/', views.notice_list, name='notice'),
+    path('portfolio/', views.portfolio_list, name='portfolio'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def notice_list(request):
+    return render(request, 'home/notice_list.html')
+
+def portfolio_list(request):
+    return render(request, 'home/portfolio_list.html')
+
+def portfolio_list2(request):
+    return render(request, 'home/portfolio_list2.html')
+
+def portfolio_list3(request):
+    return render(request, 'home/portfolio_list3.html')
