50bf67bb2bddd53573d63f9f2ee19f6007ae6256
Bora Shin <d6ymmd@gmail.com>
2020-02-12 16:41:27 +0900

    edit album-detail templates

--- home/urls.py
+++ home/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
     path('photo/', views.photo_list, name='photo_list'),
     path('address/', views.address, name='address'),
     path('practice/', views.practice, name='practice'),
+
+    # 데이터 넣기전
     path('photo/detail/', views.photo_detail, name='photo-detail'),
+    # 데이터 넣은 후
+    # path('photo/<int:pk>/', views.photo_detail, name='photo-detail'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
